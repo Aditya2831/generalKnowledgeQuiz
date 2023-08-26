f6bd463e63972f95f8c9692858690c2b4d13500b
Aditya Gupta <adinaman31@gmail.com>
2023-08-26 14:17:37 +0530

    minor modifications

---
+++ src/Data/QuizData.js
@@ -0,0 +1,256 @@
+export const QuizData = [
+    {
+        "question": "What is brackish water?",
+        "options": ["Water with salinity more than sea water", "Water with salinity less than freshwater", "Water with salinity between freshwater and seawater", "None"],
+        "answer": 3
+    },
+    {
+        "question": "Where is the Hazaribagh Plateau of the Eastern Ghats located?",
+        "options": ["West Bengal", "Jharkhand", "Odisha", "Andhra Pradesh"],
+        "answer": 2
+    },
+    {
+        "question": "What valuable resource is found in high reserves in the Hazaribagh Plateau of the Eastern Ghats?",
+        "options": ["Coal", "Iron Ore", "Copper", "Bauxite"],
+        "answer": 1
+    },
+    {
+        "question": "Which range of the Himalayas is home to the famous Gangotri and Yamunotri glaciers?",
+        "options": ["Kumaon-Garhwal", "Karakoram", "Kanchenjunga", "Pir Panjal"],
+        "answer": 1
+    },
+    {
+        "question": "Which mountain range is considered the youngest fold mountains in the world?",
+        "options": ["Andes", "Atlas", "Ural", "Himalayas"],
+        "answer": 4
+    },
+    {
+        "question": "Which destinations are connected by the Zoji La pass?",
+        "options": ["Kashmir to Jammu", "Kashmir to Kargil", "Leh to Srinagar", "Leh to Kargil"],
+        "answer": 3
+    },
+    {
+        "question": "Rohtang Pass connects Manali to which place?",
+        "options": ["Delhi", "Tibet", "Leh", "Haridwar"],
+        "answer": 3
+    },
+    {
+        "question": "Which layer of the Earth experiences the highest temperature?",
+        "options": ["Core", "Crust", "Mantle", "Lithosphere"],
+        "answer": 1
+    },
+    {
+        "question": "Which layer of the Earth has the highest density?",
+        "options": ["Inner Core", "Outer Core", "Crust", "Mantle"],
+        "answer": 1
+    }
+
+    ,
+        {
+            "question": "Which layer of the Earth has the lowest temperature?",
+            "options": ["Crust", "Mantle", "Outer Core", "Inner Core"],
+            "answer": 1
+        },
+        {
+            "question": "In which layer of the atmosphere do humans live?",
+            "options": ["Thermosphere", "Mesosphere", "Stratosphere", "Troposphere"],
+            "answer": 4
+        },
+        {
+            "question": "Which layer is responsible for Earth's magnetic field?",
+            "options": ["Crust", "Mantle", "Core", "Both B and C"],
+            "answer": 3
+        },
+        {
+            "question": "Which is the lower-most layer of the atmosphere?",
+            "options": ["Exosphere", "Mesosphere", "Stratosphere", "Troposphere"],
+            "answer": 4
+        },
+        {
+            "question": "Weather changes take place in which layer of the atmosphere?",
+            "options": ["Troposphere", "Mesosphere", "Thermosphere", "Stratosphere"],
+            "answer": 1
+        },
+        {
+            "question": "Which zone separates the troposphere and stratosphere?",
+            "options": ["Stratopause", "Tropopause", "Thermopause", "Mesopause"],
+            "answer": 2
+        },
+        {
+            "question": "Which layer of Earth's atmosphere contains electrically charged particles known as ions?",
+            "options": ["Exosphere", "Stratosphere", "Ionosphere", "Mesosphere"],
+            "answer": 3
+        },
+        {
+            "question": "Cosmology is the study of _________.",
+            "options": ["Universe", "Moons", "Sun", "Universal expansion"],
+            "answer": 1
+        },
+        {
+            "question": "The patterns formed by different groups of stars are called _________.",
+            "options": ["Big Bang", "Nebula", "Constellation", "Congregation"],
+            "answer": 3
+        },
+        {
+            "question": "Which is the thickest layer of Earth?",
+            "options": ["Mantle", "Outer Core", "Crust", "Inner Core"],
+            "answer": 1
+        },
+        {
+            "question": "Which of the following gas does not contribute to the greenhouse effect?",
+            "options": ["Water Vapour", "Carbon Dioxide", "Carbon Monoxide", "Argon"],
+            "answer": 4
+        },
+        {
+            "question": "Which microorganism in bread converts sugars into carbon dioxide and ethanol?",
+            "options": ["Yeast", "Buffering Agent", "Fungi", "Algae"],
+            "answer": 1
+        },
+        {
+            "question": "Which element in bread makes it stretchable?",
+            "options": ["Carbon Dioxide", "Ethanol", "Gluten", "Glutamic acid"],
+            "answer": 3
+        },
+        {
+            "question": "Which chemical is released from onions that makes us cry while cutting onions?",
+            "options": ["Syn-propanethial-S-oxide", "Syn-propanethail-Na-Oxide", "Syn-propanethial-C-oxide", "Trans-propanethia-P-oxide"],
+            "answer": 1
+        },
+        {
+            "question": "Which compound acts as a natural insecticide in garlic?",
+            "options": ["ADH", "Benzyl Acetate", "Aspirin", "Allicin"],
+            "answer": 4
+        },
+        {
+            "question": "Which substance is a good pH indicator that is used in daily cooking in the kitchen?",
+            "options": ["Curcumin", "Garlic Powder", "Pepper", "Chilly"],
+            "answer": 1
+        },
+        {
+            "question": "Which of the following is the function of an antipyretic drug?",
+            "options": ["Reduces depressive symptoms", "Reduces fever", "Stops bacterial infections", "Stops allergy"],
+            "answer": 2
+        },
+        {
+            "question": "The chemicals used in the formation of nail polish are ______________.",
+            "options": ["Butyl Acetate or Ethyl Acetate", "Methyl Ketone or Ethyl Ketone", "Butyl or Ethyl Carboxylic Acids", "Butyl Ketone and Ethyl Acetate"],
+            "answer": 1
+        },
+        {
+            "question": "Which of the following elements has wide importance in solar cells?",
+            "options": ["Selenium", "Uranium", "Helium", "Lead"],
+            "answer": 1
+        },
+        {
+            "question": "Which product is found in skin lightening products, bath, and fragrance products?",
+            "options": ["Ammonium Nitrate", "Amylose", "Argon", "Acetone"],
+            "answer": 4
+        },
+        {
+            "question": "Which chemical is used to give red colour in fireworks?",
+            "options": ["Copper Chloride", "Calcium Chloride", "Barium Chloride", "Strontium Carbonate"],
+            "answer": 4
+        },
+        {
+            "question": "What is an 'Economic Agent' in an Economy?",
+            "options": ["Economic decision-making individuals / institutions", "Factors of production", "Basic/raw material used for different sectors", "Indicates various inflation indicating factors"],
+            "answer": 1
+        },
+        {
+            "question": "Which goods are also known as 'Consumer durables'?",
+            "options": ["Capital goods", "Consumer goods", "Primary Goods", "Luxury Goods"],
+            "answer": 1
+        },
+        {
+            "question": "The stock of unsold finished goods, or semi-finished goods, or raw materials which a firm carries from one year to the next is called _________.",
+            "options": ["Flow", "Inventory", "Gross Value Added", "Reserves"],
+            "answer": 2
+        },
+        {
+            "question": "'Fiat Money' includes which of the following?",
+            "options": ["Gold Reserves", "Coins and Currency notes", "Foreign Reserves", "All above"],
+            "answer": 2
+        },
+        {
+            "question": "What is the process through which the central bank seeks to limit the effect of inflows and outflows of capital on the money supply?",
+            "options": ["Sterilization", "Open Market Operations", "Bank Rate adjustment", "Optimization"],
+            "answer": 1
+        },
+        {
+            "question": "The Primary Deficit in an economy will be equal to which of the following?",
+            "options": ["Revenue expenditure – Revenue receipts except Revenue used for capital", "Net borrowing at home + Borrowing from RBI + Borrowing from abroad", "Revenue expenditure – Revenue receipts", "Gross fiscal deficit – net interest liabilities"],
+            "answer": 4
+        },
+        {
+            "question": "___________ is a new Liquidity Adjustment Facility created by RBI in 2011, whose rate of interest is 1% higher than the Repo Rate.",
+            "options": ["Statutory Liquidity Ratio", "Capital to Risk Weighted Assets Ratio", "Marginal Standing Facility", "Bank Rate"],
+            "answer": 3
+        },
+        {
+            "question": "The transformation of a state-owned organisation into a corporation is known as _________.",
+            "options": ["Corporatization", "Disinvestment", "Exchange Traded Funding", "Reformation"],
+            "answer": 1
+        },
+        {
+            "question": "Which rate is the official interest rate at which RBI provides a loan to banks for the long term?",
+            "options": ["Repo Rate", "Reverse Repo Rate", "Bank Rate", "Marginal Adjustment Facility Rate"],
+            "answer": 3
+        },
+        {
+            "question": "_________ rate is the rate at which short term funds are borrowed and lent in the money market.",
+            "options": ["Call money", "Bank", "Short term", "Repo"],
+            "answer": 1
+        },
+        {
+            "question": "What is the name of the ancient Hindu tradition in which a widow is allowed to have a child with the husband's brother?",
+            "options": ["Niyoga", "Sethi", "Shaka", "Vyapti"],
+            "answer": 1
+        },
+        {
+            "question": "The Bhabru Inscriptions were found where?",
+            "options": ["Lucknow", "Chennai", "Jaipur", "Ahmedabad"],
+            "answer": 3
+        },
+        {
+            "question": "Barabar Caves are ancient caves having Ashokan inscriptions, are located in which state?",
+            "options": ["Karnataka", "Jharkhand", "Madhya Pradesh", "Bihar"],
+            "answer": 4
+        },
+        {
+            "question": "In which Major Rock Edict of Ashoka is the mention of Dhamma Mahamatas mainly found?",
+            "options": ["4th", "5th", "6th", "2nd"],
+            "answer": 2
+        },
+        {
+            "question": "Which Major Rock Edict of Ashoka mentions the victory of Ashoka over Kalinga?",
+            "options": ["8th", "9th", "13th", "11th"],
+            "answer": 3
+        },
+        {
+            "question": "The 7th and 12th Major Rock Edicts of Ashoka give information about?",
+            "options": ["Information about Dhamma", "Kalinga War", "Construction of Barabar Caves", "Tolerance of all religions"],
+            "answer": 4
+        },
+        {
+            "question": "Which officials are appointed in the Mauryan Period for the Administration of Justice?",
+            "options": ["Rajukas", "Dhammamahatras", "Vidhi Amatya", "Dauvarikas"],
+            "answer": 1
+        },
+        {
+            "question": "Which Indian King is mentioned as Sandrocottus in Greek Texts?",
+            "options": ["Ashoka", "Chandragupta Maurya", "Bindusar", "Dashratha"],
+            "answer": 2
+        },
+        {
+            "question": "Which of the following is not an essential Organ of state as per the Saptang Theory of State by Chanakya?",
+            "options": ["Swami", "Kosha", "Mitra", "Nyaya"],
+            "answer": 4
+        },
+        {
+            "question": "Which of the following scholars first deciphered the Ashokan Edicts?",
+            "options": ["Strebo", "James Princep", "Alexandra Cunningham", "Robert Sebel"],
+            "answer": 2
+        }
+    
+    
+]
